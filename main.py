26b4dadd5ec3358cf5fc32ceb92867cdb29b86f6
Thomas Wesolowski <wojoinc@iastate.edu>
2017-07-18 09:40:20 -0500

    Rolled back get_devices method to previous commit. Updated to bring it back to a working state.

--- main.py
+++ main.py
@@ -37,8 +37,39 @@ def runrestore():
     pass
 
 
-def helpwindow():  # TODO
-    pass
+def helpwindow():
+    helpwindow = Toplevel(mainwindow)
+    helplabel = Label(helpwindow, bg='firebrick2', text="All config files stored in ~/.backup_superscript\n"
+                                                        "If something is broken, please don't try to contact TJ or Mitch.\n"
+                                                        "Thanks.\n\n"
+                                                        "Github: https://github.com/WojoInc/crshop\n")
+    helplabel.pack()
+
+
+def errorwindow(message):  # TODO
+    errorwindow = Toplevel(mainwindow)
+    aboutlabel = Label(errorwindow, text=""
+                                         "              ,---------------------------,\n"
+                                         "              |  /---------------------\  |\n"
+                                         "              | |                       | |\n"
+                                         "              | |     REEEEEEE          | |\n"
+                                         "              | |      EEEEEEEE         | |\n"
+                                         "              | |       EEEEEEEE        | |\n"
+                                         "              | |                       | |\n"
+                                         "              |  \_____________________/  |\n"
+                                         "              |___________________________|\n"
+                                         "            ,---\_____     []     _______/------,\n"
+                                         "          /         /______________\           /|\n"
+                                         "        /___________________________________ /  | ___\n"
+                                         "        |                                   |   |    )\n"
+                                         "        |  o o o                 [-------]  |  /    _)_\n"
+                                         "        |__________________________________ |/     /  /\n"
+                                         "    /-------------------------------------/|      ( )/\n"
+                                         "  /-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/ /\n"
+                                         "/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/ /\n"
+                                         "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n"
+                                         + message)
+    aboutlabel.pack()
 
 
 def setdevice(value):
@@ -50,6 +81,8 @@ def changeQNAP(value):
     print(value.get())
     with open(configdir + configfile, "w+") as filetochange:
         config.write(filetochange)
+    mainwindow.mainloop()
+
 
 
 # Check for config.ini and create one if there is none
@@ -68,16 +101,9 @@ mainwindow = Tk()   # The main window for the program options to run in
 mainwindow.title("ITS Backup Superscript")
 menubar = Menu(mainwindow)  # Space for the drop-down file menu from the top
 
-# "File" cascade menu
-filemenu = Menu(menubar, tearoff=0)  # Actual drop-down file menu
-filemenu.add_command(label="Placeholder", command=placeholder())  # TODO actually add commands
-filemenu.add_separator()    # adds a separator, you dingus
-filemenu.add_command(label="other placeholder", command=placeholder())
-menubar.add_cascade(label="File", menu=filemenu)
-
 # "Advanced" cascade menu
 advmenu = Menu(menubar, tearoff=0)
-advmenu.add_command(label="About", command=helpwindow())
+advmenu.add_command(label="About", command=lambda: helpwindow())
 
 # Qnap radio buttons inside the advanced menu
 selected_qnap = IntVar()  # stores and pulls last used QNAP
@@ -88,7 +114,7 @@ qnapmenu = Menu(advmenu, tearoff=0)
 qnapmenu.add_radiobutton(label="QNAP_1", variable=selected_qnap, value=1, command=lambda: changeQNAP(selected_qnap))
 qnapmenu.add_radiobutton(label="QNAP_2", variable=selected_qnap, value=2, command=lambda: changeQNAP(selected_qnap))
 advmenu.add_cascade(label="Change QNAP", menu=qnapmenu)
-menubar.add_cascade(label="Advanced", menu=advmenu)
+menubar.add_cascade(label="File", menu=advmenu)
 
 
 mainwindow.config(menu=menubar)  # Packs the menubar
@@ -113,8 +139,7 @@ devices = backup.get_devices()
 devicedict = {}
 
 for device in devices:  # Retrives the first part of devices from backup.py
-    # devicedict[device[0]] = device
-    devicedict[device[0] + " " + str(device[1]) + " " + str(device[2])] = device
+    devicedict[device[0] + "    " + device[1] + "   " + device[2]] = device
 
 
 def drive_select(value):
@@ -136,7 +161,7 @@ R1.grid(row=0, column=2)
 R2 = Radiobutton(backuptab, text="Users Folder", variable=radio_sel, value="/Users/")
 R2.grid(row=1, column=2)
 
-R3 = Radiobutton(backuptab, text="Other:", variable=radio_sel, value="")  # TODO add other text box
+R3 = Radiobutton(backuptab, text="Other:", variable=radio_sel, value="")  # TODO get value from text box
 R3.grid(row=2, column=2)
 
 otherentry = Text(backuptab, height=1, width=35)
@@ -145,10 +170,10 @@ otherentry.grid(row=2, column=3)
 startbutton = Button(backuptab, text="Start Backup on QNAP-" + str(selected_qnap.get()), command=lambda:
 runbackup(selected_device[1], selected_device[2], radio_sel.get()))
 
-startbutton.grid(row=5, column=3, padx=5, pady=15)
+startbutton.grid(row=3, column=3, padx=5, pady=15)
 
-
-restoretab = ttk.Frame(tabs, height=400, width=800)  # Tab with options for restoring
+# Restoring
+restoretab = ttk.Frame(tabs, height=400, width=800)
 # Create entry for ticket number
 ticketlabel = Label(restoretab, text="Ticket #:")
 ticketlabel.grid(row=0, column=0)
